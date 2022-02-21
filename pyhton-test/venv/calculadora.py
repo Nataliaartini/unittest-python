23231f04d76c39596990b40d97a75c5d63b0f80f
Natalia <nataliaartini@hotmail.com>
2022-02-21 17:44:12 -0300

    adiciona assertion error

--- pyhton-test/venv/calculadora.py
+++ pyhton-test/venv/calculadora.py
@@ -1,3 +1,4 @@
 def soma(x,y):
     assert isinstance(x, (int, float)), "X precisa ser um numero natural ou decimal!"
+    assert isinstance(y, (int, float)), "Y precisa ser um numero natural ou decimal!"
     return x+y
