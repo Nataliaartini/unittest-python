5608dbdd9c046618cf9a84483fd41eaccfaaf488
Natalia <nataliaartini@hotmail.com>
2022-02-21 17:50:45 -0300

    teste errado direto na main

--- pyhton-test/venv/calculadora.py
+++ pyhton-test/venv/calculadora.py
@@ -1,4 +1,17 @@
 def soma(x,y):
+    """
+
+    >>> soma(10,20)
+    31
+
+    """
+
+
     assert isinstance(x, (int, float)), "X precisa ser um numero natural ou decimal!"
     assert isinstance(y, (int, float)), "Y precisa ser um numero natural ou decimal!"
     return x+y
+
+
+if __name__== "__main__":
+    import doctest
+    doctest.testmod()
