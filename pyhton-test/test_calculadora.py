222f22c4a4636b371e49d95fae43de84b350147d
Natalia <nataliaartini@hotmail.com>
2022-02-23 16:44:02 -0300

    teste com assertion error no unittest

--- pyhton-test/test_calculadora.py
+++ pyhton-test/test_calculadora.py
@@ -6,11 +6,19 @@ class TestCalculadora(unittest.TestCase):
     #     print("nao vai axecutar no teste")
 
 
-    def test_soma_5_e_5_retorna_10(self):
-        self.assertEqual(soma(5, 5), 10)
+    def test_soma_varias_entradas(self):
+        x_y_saidas = (
+            (20, 30, 50),
+            (1, 6.5, 7.5),
+            (10, 52, 62),
+            (-10, 52, 42),
+        )
 
-    def test_soma_5_e_menos_5_retorna_0(self):
-        self.assertEqual(soma(-5, 5), 0)
+        for x_y_saida in x_y_saidas:
+            with self.subTest(x_y_saida=x_y_saida):
+                x, y, saida = x_y_saida
+                self.assertEqual(soma(x,y), saida)
 
+    def test_soma_x_nao_e_int_ou_float(self):
 
-unittest.main()
+unittest.main(verbosity=2)
