f654e14d342947c37de9cb46ae522a6e83e0e9bd
Natalia <nataliaartini@hotmail.com>
2022-02-23 15:27:33 -0300

    utilizando doctest

--- pyhton-test/venv/calculadora.py
+++ pyhton-test/venv/calculadora.py
@@ -1,3 +1,6 @@
+from ntpath import join
+
+
 def soma(x,y):
     """
 
@@ -14,6 +17,16 @@ def soma(x,y):
     assert isinstance(x, (int, float)), "X precisa ser um numero natural ou decimal!"
     assert isinstance(y, (int, float)), "Y precisa ser um numero natural ou decimal!"
     return x+y
+
+
+def subtrai(a,b):
+    """ 
+    >>> subtrai(6,5)
+    1
+    >>> subtrai(95,12)
+    83
+    """
+    return a-b
 
 
 if __name__== "__main__":
--- pyhton-test/venv/main.py
+++ pyhton-test/venv/main.py
@@ -1,6 +1,11 @@
-from calculadora import soma
+from calculadora import soma, subtrai
 
 try:
     print(soma("98745",4987))
+except AssertionError as e:
+    print(f"conta inválida: {e}")
+
+try:
+    print(subtrai(4937,4987))
 except AssertionError as e:
     print(f"conta inválida: {e}")
