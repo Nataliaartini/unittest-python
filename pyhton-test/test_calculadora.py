0d28eff5bbf4c812c36e777b17660e3e6a8dc83a
Natalia <nataliaartini@hotmail.com>
2022-02-23 15:54:11 -0300

    utilizando unittest

--- pyhton-test/test_calculadora.py
+++ pyhton-test/test_calculadora.py
@@ -2,12 +2,16 @@ import unittest
 from calculadora import soma, subtrai
 
 class TestCalculadora(unittest.TestCase):
-    def fala(self):
-        print("nao vai axecutar no teste")
+    # def fala(self):
+    #     print("nao vai axecutar no teste")
 
 
     def test_soma_5_e_5_retorna_10(self):
         self.assertEqual(soma(5, 5), 10)
 
+    def test_soma_5_e_menos_5_retorna_0(self):
+        self.assertEqual(soma(-5, 5), 0)
 
-unittest.main(verbosity=1)
+
+
+unittest.main()
