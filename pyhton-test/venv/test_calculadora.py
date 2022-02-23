7fb99e67b74164bdebfc0e5baa3defcbd4b3985c
Natalia <nataliaartini@hotmail.com>
2022-02-23 16:52:00 -0300

    corrigindo diretorio

---
+++ pyhton-test/venv/test_calculadora.py
@@ -0,0 +1,31 @@
+import unittest
+from calculadora import soma, subtrai
+
+class TestCalculadora(unittest.TestCase):
+    # def fala(self):
+    #     print("nao vai axecutar no teste")
+
+
+    def test_soma_varias_entradas(self):
+        x_y_saidas = (
+            (20, 30, 50),
+            (1, 6.5, 7.5),
+            (10, 52, 62),
+            (-10, 52, 42),
+        )
+
+        for x_y_saida in x_y_saidas:
+            with self.subTest(x_y_saida=x_y_saida):
+                x, y, saida = x_y_saida
+                self.assertEqual(soma(x,y), saida)
+
+    def test_soma_x_nao_e_int_ou_float(self):
+        with self.assertRaises(AssertionError):
+            soma("11", 2)
+
+    def test_soma_y_nao_e_int_ou_float(self):
+        with self.assertRaises(AssertionError):
+            soma(2, "2")
+
+
+unittest.main(verbosity=2)
