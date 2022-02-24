23b7e26200398af6c0afa7aecea918e3d0d9cba7
Natalia <nataliaartini@hotmail.com>
2022-02-24 08:55:00 -0300

    adicionando passo para verificacao do teste

--- pyhton-test/venv/cafedamanha.py
+++ pyhton-test/venv/cafedamanha.py
@@ -1,17 +1,20 @@
-"""
- recebe um numero inteiro
- verifica se o numero é multiplo de 3 e 5:
-    cafezinho diliça
- verifica se é somente multiplo de 3:
-    cafezinho
- verifica se é somente multiplo de 5:
-    diliça
- verifica se não é multiplo nem de 3 nem de 5:
-    café solúvel pra ti
-"""
+
+# -recebe um numero inteiro
+# -verifica se o numero é multiplo de 3 e 5:
+#     cafezinho diliça
+# -verifica se é somente multiplo de 3:
+#     cafezinho
+# -verifica se é somente multiplo de 5:
+#     diliça
+# -verifica se não é multiplo nem de 3 nem de 5:
+#     café solúvel pra ti
+
 
 from ast import Assert
 
 
 def cafe_da_manha(n):
     assert isinstance(n, int), "n deve ser inteiro"
+
+    if n % 3 == 0 and n % 5 == 0:
+        return "cafezinho diliça"
--- pyhton-test/venv/test_cafedamanha.py
+++ pyhton-test/venv/test_cafedamanha.py
@@ -1,16 +1,10 @@
-"""
-TDD - Test Driven Development (desenvolvimento dirigido a testes)
-
-RED:
- criar o teste e vê a falha 
-
-GREEN:
-* criar o codigo e vê o teste passar
-
-REFACTOR:
- melhorar o código
-
-"""
+# TDD - Test Driven Development (desenvolvimento dirigido a testes)
+# RED
+# -criar o teste e vê a falha 
+# GREEN
+# -criar o codigo e vê o teste passar
+# REFACTOR
+# -melhorar o código
 
 import unittest
 from cafedamanha import cafe_da_manha
@@ -19,5 +13,13 @@ class TestCafeDaManha(unittest.TestCase):
     def test_cafe_da_manha_assertion_error_se_nao_for_int(self):
         with self.assertRaises(AssertionError):
             cafe_da_manha("0")
+
+    def test_cafe_da_manha_retorna_cafe_soluvel_se_n_for_multiplo_de_3_e_5(self):
+        entradas = (15, 30, 45, 60)
+        saida = "cafezinho diliça"
+        for i in entradas:
+            with self.subTest(i = i, saida=saida):
+                self.assertEqual(cafe_da_manha(i), saida, msg=f"{i} nao retornou {saida}")
+
 
 unittest.main(verbosity=2)            
