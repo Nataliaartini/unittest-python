02de2012ef20396b890066fbb5c9dfa57c592ff0
Natalia <nataliaartini@hotmail.com>
2022-02-28 14:44:18 -0300

    utilizando mypy para otimizaçao de codigo e tempo programando

--- pyhton-test/venv/pessoa.py
+++ pyhton-test/venv/pessoa.py
@@ -1,11 +1,12 @@
 import requests
 
+
 class Pessoa:
     def __init__(self, nome, sobrenome):
         self.nome = nome
         self.sobrenome = sobrenome
         self.dados_obtidos = False
-    
+
     def obteve_todos_os_dados(self):
         resposta = requests.get("https://google.com")
 
